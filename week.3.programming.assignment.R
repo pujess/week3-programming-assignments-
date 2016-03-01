cb4a819d936b15f09face06e6a4f1231c716f850
pujess <pujess@yahoo.com>
2016-03-01 13:19:10 +0800

    Added files via upload

---
+++ week.3.programming.assignment.R
@@ -0,0 +1,31 @@
+##########################
+makeCacheMatrix <- function(x = numeric()) {
+  inver<- NULL
+  set <- function(y) {
+    x <<- y
+    inver<<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inver <<- inverse
+  getInverse <- function()inver
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+
+################ programming assignment 
+cacheSolve <- function(x, ...) {
+  inver <- x$getInverse()
+  if(!is.null(inver))   {
+    message("getting cached data")
+    return(inver)
+  }
+  mat<-x$get
+  inver <- solve(mat, ...)
+  x$setInverse(inver)
+  inver
+  
+  
+}
